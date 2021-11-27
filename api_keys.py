4c395b0f45cd41f268b9ac7c5995f6e6899de77e
Chris Christensen <chrischristensen21@gmail.com>
2021-11-26 23:10:17 -0700

    weatherpy finished and some of vacation py

--- api_keys.py
+++ api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = "b7477a923ba2080da810f0552a9aa2d3"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = "AIzaSyATBh0c7M-WyQhH6lHIjsn9ftN-XsKSDiU"
